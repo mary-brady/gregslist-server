60d4b1108093f99d5e2aa4870fcb37b9037b2427
Mary Brady <emceebrady@gmail.com>
2018-08-21 16:04:51 -0600

    jobs and house routes

--- server-assets/routes/houses-routes.js
+++ server-assets/routes/houses-routes.js
@@ -1,10 +1,44 @@
 const router = require('express').Router()
 let House = require('../models/House')
 
+router.get('/:id?', (req, res, next) => {
+    if (!req.params.id) {
+        House.find({})
+            .then(houses => {
+                return res.send(houses)
+            })
+    }
+    House.findById(req.params.id)
+        .then(house => {
+            res.send(house)
+        })
+})
 
+router.post('/', (req, res, next) => {
+    let newHouse = req.body
+    House.create(newHouse)
+        .then(house => {
+            res.send(house)
+        })
+        .catch(err => {
+            res.status(400).send(err)
+        })
+})
 
+router.put('/:id', (req, res, next) => {
+    House.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    })
+        .then(newHouse => {
+            res.send(newHouse)
+        })
+})
 
-
-
+router.delete('/:id', (req, res, next) => {
+    House.findByIdAndRemove(req.params.id)
+        .then(oldDeletedHouse => {
+            res.send('BUHLETED')
+        })
+})
 
 module.exports = router
--- server-assets/routes/jobs-routes.js
+++ server-assets/routes/jobs-routes.js
@@ -1,10 +1,45 @@
 const router = require('express').Router()
 let Job = require('../models/Job')
 
+router.get('/:id?', (req, res, next) => {
+    if (!req.params.id) {
+        Job.find({})
+            .then(jobs => {
+                return res.send(jobs)
+            })
+    }
+    Job.findById(req.params.id)
+        .then(job => {
+            res.send(job)
+        })
+})
 
+router.post('/', (req, res, next) => {
+    let newJob = req.body
+    Job.create(newJob)
+        .then(job => {
+            res.send(job)
+        })
+        .catch(err => {
+            res.status(400).send(err)
+        })
+})
 
+router.put('/:id', (req, res, next) => {
+    Job.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    })
+        .then(newJob => {
+            res.send(newJob)
+        })
+})
 
-
+router.delete('/:id', (req, res, next) => {
+    Job.findByIdAndRemove(req.params.id)
+        .then(oldDeletedJob => {
+            res.send('BUHLETED')
+        })
+})
 
 
 module.exports = router
