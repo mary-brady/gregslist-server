3e35a15452daa1db9856ef061e95e129f1e842d1
Mary Brady <emceebrady@gmail.com>
2018-08-21 15:32:25 -0600

    car routes working

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ let server = express()
 let bp = require('body-parser')
 let port = 3000
 
+//connect to DB
+require('./server-assets/db/mlab-config')
+
+//Register middleware
 server.use(bp.json())
 server.use(bp.urlencoded({
     extended: true
 }))
 
+//Routes
+let carRoutes = require('./server-assets/routes/cars-routes')
+let houseRoutes = require('./server-assets/routes/houses-routes')
+let jobRoutes = require('./server-assets/routes/jobs-routes')
+
+server.use('/cars/', carRoutes)
+server.use('/houses/', houseRoutes)
+server.use('/jobs/', jobRoutes)
 
 server.listen(port, () => {
     console.log('Runnin on port:', port)
--- server-assets/routes/cars-routes.js
+++ server-assets/routes/cars-routes.js
@@ -1,10 +1,44 @@
 const router = require('express').Router()
 let Car = require('../models/Car')
 
+router.get('/:id?', (req, res, next) => {
+    if (!req.params.id) {
+        Car.find({})
+            .then(cars => {
+                return res.send(cars)
+            })
+    }
+    Car.findById(req.params.id)
+        .then(car => {
+            res.send(car)
+        })
+})
 
+router.post('/', (req, res, next) => {
+    let newCar = req.body
+    Car.create(newCar)
+        .then(car => {
+            res.send(car)
+        })
+        .catch(err => {
+            res.status(400).send(err)
+        })
+})
 
+router.put('/:id', (req, res, next) => {
+    Car.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    })
+        .then(newCar => {
+            res.send(newCar)
+        })
+})
 
-
-
+router.delete('/:id', (req, res, next) => {
+    Car.findByIdAndRemove(req.params.id)
+        .then(oldDeletedCar => {
+            res.send('BUHLETED')
+        })
+})
 
 module.exports = router
