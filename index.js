c707172ad4c28b2940e59cfee79d40fd07bd716b
Mary Brady <emceebrady@gmail.com>
2018-08-21 13:40:49 -0600

    project setup

---
+++ index.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let server = express()
+let bp = require('body-parser')
+let port = 3000
+
+server.use(bp.json())
+server.use(bp.urlencoded({
+    extended: true
+}))
+
+
+
+
+
+
+
+
+
+
+
+
+server.listen(port, () => {
+    console.log('Runnin on port:', port)
+})
